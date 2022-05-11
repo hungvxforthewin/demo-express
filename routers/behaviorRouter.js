905d276562effa2d6e7a0b1b159f3d191774ed42
hungvx <vuxuanhungmta@gmail.com>
2022-05-11 09:16:55 +0700

    install, downgrade version fetch-node

--- routers/behaviorRouter.js
+++ routers/behaviorRouter.js
@@ -5,7 +5,10 @@
  */
 const express = require("express");
 const router = express.Router();
+const fetch = require("node-fetch");
 
+//const fetch2 = require("fetch")
+// const fetchUrl = require("fetch").fetchUrl;
 /**
  * Init all APIs on your application
  * @param {*} app from express
@@ -17,6 +20,59 @@ const retailAPIs = (app) => {
     });
     app.post("/login", function (req, res) {
         res.json({ mess: "login from api web retail" });
+    });
+    // GET
+    // npm fetch
+    // fetchUrl("https://jsonplaceholder.typicode.com/todos/1", function (error, meta, body) {
+    //     console.log(body.toString());
+    // });
+
+    // fetch
+    //     .fetchUrl("https://jsonplaceholder.typicode.com/todos/1")
+    //     .then((response) => response.json())
+    //     .then((data) => {
+    //         console.log(data); // Prints result from `response.json()` in getRequest
+    //     })
+    //     .catch((error) => console.error(error));
+
+    // async function check() {
+    //     const data = await fetch("https://jsonplaceholder.typicode.com/todos/1")
+    //         .then((response) => response.json())
+    //         .then((data) => {
+    //             console.log(data); // Prints result from `response.json()` in getRequest
+    //         })
+    //         .catch((error) => console.error(error));
+    // }
+    // check();
+
+    // npm node-fetch
+
+    app.get("/get", async (req, res) => {
+        // Promise then
+        // fetch("https://jsonplaceholder.typicode.com/todos/1")
+        //     .then((response) => response.json())
+        //     .then((data) => {
+        //         console.log(data); // Prints result from `response.json()` in getRequest
+        //         res.json(data);
+        //     })
+        //     .catch((error) => console.error(error));
+
+        // async/await
+        const data = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+        const result = await data.json();
+        console.log(result);
+        return res.json(result);
+    });
+
+    async function fetchMoviesJSON() {
+        const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+        const movies = await response.json();
+        return movies;
+    }
+    const testData = fetchMoviesJSON();
+    console.log(testData);
+    fetchMoviesJSON().then((movies) => {
+        console.log(movies); // fetched movies
     });
 
     return app.use("/", router); //default
