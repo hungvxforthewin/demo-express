1334ed1322e3c0de706582765af68d89724ae2ee
hungvx <vuxuanhungmta@gmail.com>
2022-05-13 08:54:48 +0700

    mock data

--- middleware/PassportJwtMiddleware.js
+++ middleware/PassportJwtMiddleware.js
@@ -7,18 +7,28 @@ exports.applyPassportStrategy = (passport) => {
     options.secretOrKey = config.passport.secret;
     passport.use(
         new Strategy(options, (payload, done) => {
-            User.findOne({ email: payload.email }, (err, user) => {
-                if (err) {
-                    return done(err, false);
-                }
-                if (user) {
-                    return done(null, {
-                        email: user.email,
-                        _id: user[underscoreId],
-                    });
-                }
-                return done(null, false);
-            });
+            // hard code, mock data
+            if (payload.email === "hungvx@gmail.com") {
+                return done(null, {
+                    email: "hungvx@gmail.com",
+                    _id: 1,
+                });
+            } else {
+                return done(err, false);
+            }
+            // query with database
+            // User.findOne({ email: payload.email }, (err, user) => {
+            //     if (err) {
+            //         return done(err, false);
+            //     }
+            //     if (user) {
+            //         return done(null, {
+            //             email: user.email,
+            //             _id: user[underscoreId],
+            //         });
+            //     }
+            //     return done(null, false);
+            // });
         })
     );
 };
